c6160429b7447c011df7b342c1044c82fbd363fb
J-XZ <jiangxuzhen@hhu.edu.cn>
2024-05-19 12:44:03 +0000

    tmp

--- kill_all.fish
+++ kill_all.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/fish
-
+ps aux | grep mongo | grep -v grep | grep -v kill_all
 # 使用 ps 命令查找所有 mongod 进程
-set mongod_pids (ps aux | grep mongo | grep -v grep | awk '{print $2}')
+set mongod_pids (ps aux | grep mongo | grep -v grep | grep -v kill_all | awk '{print $2}')
 
 # 遍历所有进程并逐个关闭
 for pid in $mongod_pids
